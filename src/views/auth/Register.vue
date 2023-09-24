57eb8ee426fc010b7f7fc91dbe53f23713d81caa
aymansohag <aymansohag.contact@gmail.com>
2023-09-25 00:10:15 +0600

    otp form show

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -24,22 +24,26 @@
     const auth = userAuth();
     const router = useRouter();
 
-    // Api Call Via store (Auth
+    // Api Call Via store (Auth)
     const onSubmit = async (values, { setErrors }) => {
         const res = await auth.register(values);
         if (res.data) {
-            router.push({ name: 'home'});
-            ElNotification({
-                title: 'Congrats',
-                message: 'Register Successfully',
-                type: 'success',
-                position: 'top-left',
-            })
+          sendOtp.value = true;
         } else {
             setErrors(res);
         }
     };
+    
+    const sendOtp = ref(false);
 
+    // Sms verificaiton 
+    const otpSchema = yup.object({
+        otp_code: yup.number().required(),
+    });
+
+    const onOtpSubmit = async (values, { setErrors }) => {
+
+    };
 </script>
 <template>
     <div>
@@ -49,10 +53,11 @@
                     <div class="col-12 col-sm-10 col-md-12 col-lg-12 col-xl-6">
                         <div class="user-form-card">
                             <div class="user-form-title">
-                                <h2>Customer Registration</h2>
+                                <h2 v-if="sendOtp">Verify Your Mo bile</h2>
+                                <h2 v-else>Customer Registration</h2>
                                 <p>Use your required information</p>
                             </div>
-                            <div class="user-form-group" id="axiosForm">
+                            <div class="user-form-group" v-if="!sendOtp">
                                 <Form class="user-form" @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
                                     <div class="row">
                                         <div class="col-md-6">
@@ -69,7 +74,7 @@
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <Field type="text" name="email" class="form-control" :class="{'is-invalid' : errors.email}" placeholder="Email" />
+                                        <Field type="email" name="email" class="form-control" :class="{'is-invalid' : errors.email}" placeholder="Email" />
                                         <span class="text-danger" v-if="errors.email">{{ errors.email }}</span>
                                     </div>
                                     <div class="row">
@@ -94,11 +99,24 @@
                                     </div>
                                 </Form>
                             </div>
+
+                            <div class="user-form-group" v-else>
+                                <Form class="user-form" @submit="onOtpSubmit" :validation-schema="otpSchema" v-slot="{ errors, isSubmitting }">
+                                    <div class="form-group">
+                                        <Field type="number" name="otp_code" class="form-control" :class="{'is-invalid' : errors.otp_code}" placeholder="Otp Code" />
+                                        <span class="text-danger" v-if="errors.otp_code">{{ errors.otp_code }}</span>
+                                    </div>
+                                    <div class="form-button">
+                                        <button type="submit" :disabled="isSubmitting">Verify Otp 
+                                            <span v-show="isSubmitting" class="spinner-border spinner-border-sm ms-1"></span>
+                                        </button>
+                                    </div>
+                                </Form>
+                            </div>
                         </div>
                         <div class="user-form-remind">
                             <p>
-                                Already Have An Account? <router-link :to="{ name: 'user.login' }">Login
-                                    Here</router-link>
+                                Already Have An Account? <router-link :to="{ name: 'user.login' }">Login Here</router-link>
                             </p>
                         </div>
                         <div class="user-form-footer"></div>
