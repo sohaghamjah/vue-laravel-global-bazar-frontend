a5d27d88bfe4b01af20349c2a5ae1b515155908c
aymansohag <aymansohag.contact@gmail.com>
2024-03-04 23:27:00 +0600

    auto logout

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -8,13 +8,21 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(function (config) {
     const authInfo = userAuth();
     const AUTH_TOKEN = authInfo.user.meta ? `Bearer ${authInfo.user.meta.token}` : "";
-
     config.headers.common["Authorization"] = AUTH_TOKEN;
-    
     return config;
   }, function (error) {
     return Promise.reject(error);
 });
 
+// Auto logout
+axiosInstance.interceptors.response.use( (response) => {
+ return response;
+}, (error) => {
+  if(error.response && error.response.status == 401){
+    const authInfo = userAuth();
+    authInfo.user = {}
+  }
+  return Promise.reject(error);
+});
   
 export default axiosInstance;
